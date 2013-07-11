9601ee646798f684ff584e3536018446288ea043
Henrotte Julien <hoshi.graphi@gmail.com>
2013-07-11 13:43:27 +0200

    Bug fix condition

--- js/profily-plugin.js
+++ js/profily-plugin.js
@@ -13,61 +13,61 @@
         	//Get the canvas, width + height
         	var canvas = document.createElement('canvas');
 			//Get full name
-			if (settings.attribute){
+			if (settings.attribute && $(this).attr(settings.attribute) != '' && $(this).attr('data-image') != 1){
 				fullName = $(this).attr(settings.attribute);
+
+				//Retrieve canvas widht and height
+				canvas.width = $(this).width(),
+				canvas.height = $(this).height();
+
+				//Get the initials
+				var firstNameInitiales = fullName.split(' ').slice(0, -1).join(' ').charAt(0);
+				var lastNameInitiales = fullName.split(' ').slice(-1).join(' ').charAt(0);
+
+				//base on 2 string, firstName can be empty
+				if(firstNameInitiales == ''){
+					var initiales = lastNameInitiales;
+					var moduloResult = (lastNameInitiales.charCodeAt(0)) % 10;
+				}else{
+					var initiales = firstNameInitiales + lastNameInitiales;
+					var moduloResult = (firstNameInitiales.charCodeAt(0) + lastNameInitiales.charCodeAt(0)) % 10;
+				}
+
+				//get color from initiales and apply color
+				//Use modulo from charCode of initial
+				//Same color each time, color change with initiales
+				var colorMod = ["#FFD213","#cc3333","#4DBA61","#8B2786","#A85B34","#ff6600","#0099cc","#3A4769","#433225","#35384D"];
+
+				//Font + align center and middle
+				var context = canvas.getContext('2d');
+				
+
+				//Get the fonsize, based on canvas width
+				var fontSize = canvas.width / 2 + 'px';
+				if(settings.fontFamily){
+					context.font = fontSize + ' ' + settings.fontFamily;	
+				}
+				
+				context.textAlign = 'center';
+				context.textBaseline = 'middle';
+				
+				var colorCheck = colorMod[moduloResult].replace("#", '');
+				
+				//Check if the color is light or dark 
+				context.fillStyle = isTooLightYIQ(colorCheck) ? '#31323a' : '#fff';
+				
+				//Construct canvas
+				x = canvas.width / 2,
+				y = canvas.height / 2;
+				context.fillText(initiales, x, y);
+
+				//Change canvas to a base64 data uri (simple manipulation)
+				canvas = canvas.toDataURL();
+
+				//Add color to the canvas and base64 src
+				$(this).css({"background-color" : colorMod[moduloResult] });
+				$(this).attr("src",canvas);
 			}
-
-			//Retrieve canvas widht and height
-			canvas.width = $(this).width(),
-			canvas.height = $(this).height();
-
-			//Get the initials
-			var firstNameInitiales = fullName.split(' ').slice(0, -1).join(' ').charAt(0);
-			var lastNameInitiales = fullName.split(' ').slice(-1).join(' ').charAt(0);
-
-			//base on 2 string, firstName can be empty
-			if(firstNameInitiales == ''){
-				var initiales = lastNameInitiales;
-				var moduloResult = (lastNameInitiales.charCodeAt(0)) % 10;
-			}else{
-				var initiales = firstNameInitiales + lastNameInitiales;
-				var moduloResult = (firstNameInitiales.charCodeAt(0) + lastNameInitiales.charCodeAt(0)) % 10;
-			}
-
-			//get color from initiales and apply color
-			//Use modulo from charCode of initial
-			//Same color each time, color change with initiales
-			var colorMod = ["#FFD213","#cc3333","#4DBA61","#8B2786","#A85B34","#ff6600","#0099cc","#3A4769","#433225","#35384D"];
-
-			//Font + align center and middle
-			var context = canvas.getContext('2d');
-			
-
-			//Get the fonsize, based on canvas width
-			var fontSize = canvas.width / 2 + 'px';
-			if(settings.fontFamily){
-				context.font = fontSize + ' ' + settings.fontFamily;	
-			}
-			
-			context.textAlign = 'center';
-			context.textBaseline = 'middle';
-			
-			var colorCheck = colorMod[moduloResult].replace("#", '');
-			
-			//Check if the color is light or dark 
-			context.fillStyle = isTooLightYIQ(colorCheck) ? '#31323a' : '#fff';
-			
-			//Construct canvas
-			x = canvas.width / 2,
-			y = canvas.height / 2;
-			context.fillText(initiales, x, y);
-
-			//Change canvas to a base64 data uri (simple manipulation)
-			canvas = canvas.toDataURL();
-
-			//Add color to the canvas and base64 src
-			$(this).css({"background-color" : colorMod[moduloResult] });
-			$(this).attr("src",canvas);
 		});
 	}
 
